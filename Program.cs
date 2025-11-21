5dff9ac0f1ea90a16712a0e67431c28f8ec8ef80
Carlos Roberto Domingues <crdpc@yahoo.com.br>
2025-11-20 21:21:39 -0300

    Initialize ASP.NET Core Web API with Swagger

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Serviços
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "My API",
+        Version = "v1",
+        Description = "Exemplo ASP.NET Core Web API com Swagger para Azure"
+    });
+});
+
+var app = builder.Build();
+
+// Middleware
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+    c.RoutePrefix = "swagger"; // URL será /swagger/index.html
+});
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
